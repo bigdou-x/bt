2742626fe98ed433a1ead9aa4ab2771625cb9970
BigDou <1608620445@qq.com>
2019-10-02 22:56:19 +0800

    提交全局自动注册components目录下组件，axios配置

--- btxd/src/main.js
+++ btxd/src/main.js
@@ -9,7 +9,42 @@ import axios from "axios";
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
-Vue.prototype.axios = axios;
+axios.defaults.baseURL="http://127.0.0.1:8080";
+Vue.prototype.$axios = axios;
+
+const requireComponent = require.context(
+  // 其组件目录的相对路径
+  './components',
+  // 是否查询其子目录
+  false,
+  // 匹配基础组件文件名的正则表达式
+  /[A-Z]\w+\.(vue|js)$/
+)
+requireComponent.keys().forEach(fileName => {
+    console.info(fileName);
+  // 获取组件配置
+  const componentConfig = requireComponent(fileName)
+
+  // 获取组件的 PascalCase 命名
+  const componentName = upperFirst(
+    camelCase(
+      // 获取和目录深度无关的文件名
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+
+  // 全局注册组件
+  Vue.component(
+    componentName,
+    // 如果这个组件选项是通过 `export default` 导出的，
+    // 那么就会优先使用 `.default`，
+    // 否则回退到使用模块的根。
+    componentConfig.default || componentConfig
+  )
+})
 
 /* eslint-disable no-new */
 new Vue({
